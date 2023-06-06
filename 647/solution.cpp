9ea33c8e00ead2e41a333559bf791cbc449d2aa6
wyf0722 <522022230116@smail.nju.edu.cn>
2023-06-06 18:44:28 +0800

    ...

---
+++ 647/solution.cpp
@@ -0,0 +1,8 @@
+#include "leetcode.h"
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+
+    }
+};
---
+++ 72/solution.cpp
@@ -0,0 +1,8 @@
+#include "leetcode.h"
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+
+    }
+};
